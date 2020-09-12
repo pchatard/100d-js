dd97e9f5981baa8137e475938521fafbfa8dc32a
pchatard <chatard.pierre@gmail.com>
2020-09-12 21:56:56 +0200

    spinalCase

---
+++ JS/Algorithms/Intermediate Algorithm Scripting/spinalCase.js
@@ -0,0 +1,16 @@
+// Spinal Tap Case
+
+/*
+Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+*/
+
+function spinalCase(str) {
+    // See regex first
+}
+
+// Tests
+console.log(spinalCase("This Is Spinal Tap")); // should return "this-is-spinal-tap".
+console.log(spinalCase("thisIsSpinalTap")); // should return "this-is-spinal-tap".
+console.log(spinalCase("The_Andy_Griffith_Show")); // should return "the-andy-griffith-show".
+console.log(spinalCase("Teletubbies say Eh-oh")); // should return "teletubbies-say-eh-oh".
+console.log(spinalCase("AllThe-small Things")); // should return "all-the-small-things".
